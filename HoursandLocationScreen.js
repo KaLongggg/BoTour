e88d3e57e9fd3f04645829d6ae5ceef5996ee37c
KaLongggg <kalong551@gmail.com>
2023-03-21 22:52:23 +1100

    Add icon, loading page

--- HoursandLocationScreen.js
+++ HoursandLocationScreen.js
@@ -9,8 +9,6 @@ export default function HoursandLocationScreen() {
       <Text style={{ fontSize: 24, padding: 10  }}>Temporary traffic changes:</Text>
       <Text style={{ fontSize: 15, padding: 10 }}>Transport for NSW will carry out temporary traffic management activities around the University of Wollongong (UOW) and TAFE NSW from Monday 27 February 2023, to reduce congestion and ensure the safety of road users. Traffic Management will be in place weekday mornings from 7.45am to 9.15am for around six weeks. For more details visit the NSW Transport website.</Text>
       <Text style={{ fontSize: 24, padding: 10 }}>Entry to Wollongong Botanic Garden is free!</Text>
-
-
     </View>
   );
 }
