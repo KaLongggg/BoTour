caa3a475caf3b81781e8b2ea34d7103d8ef9ea52
Kobe666888 <127716801+Kobe666888@users.noreply.github.com>
2023-04-05 20:24:19 +0800

    Update AboutScreen.js

--- AboutScreen.js
+++ AboutScreen.js
@@ -1,26 +1,15 @@
 import React from 'react';
-import {View, Text, StyleSheet, ScrollView} from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 
 const About = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Text style={styles.header}>About Us</Text>
-      <Text style={styles.paragraph}>
-        Set on 27 hectares at the foot of Mount Keira, Wollngong Botanic garden is a short distance from 
-        the City Central, and next to the University of Wollngong's main campus. We are located approximately 85 
-        kilometres south of Sydney.
+      <Image source={require("./assets/about.png")} style={{ width: "100%", height: 200 }} ></Image>
+
+      <Text style={styles.text}>
+        Set on 27 hectares at the foot of Mount Keira, Wollongong Botanic Garden is a short distance from the City Centre, and next to the University of Wollongong's main campus. We are located approximately 85 kilometres south of Sydney.
       </Text>
-      <Text style={styles.paragraph}>
-        Our mission is to empower businesses with innovative and efficient
-        software solutions, helping them achieve their goals and stay ahead of
-        the competition. We strive for excellence in everything we do and
-        always put our customers first.
-      </Text>
-      <Text style={styles.paragraph}>
-        If you have any questions or would like to learn more about our
-        services, please feel free to contact us. We'd be more than happy to
-        assist you.
-      </Text>
+
     </ScrollView>
   );
 };
@@ -43,8 +32,9 @@ const styles = StyleSheet.create({
     marginBottom: 4,
   },
   text: {
-    fontSize: 16,
+    fontSize: 18,
     lineHeight: 24,
+    marginTop: 10
   },
   link: {
     fontSize: 16,
