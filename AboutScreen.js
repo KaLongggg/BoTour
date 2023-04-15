578413430d60cd6ffd870ffd935e6c91f46a6884
Kobe666888 <127716801+Kobe666888@users.noreply.github.com>
2023-04-15 22:06:38 +0800

    Update AboutScreen.js

--- AboutScreen.js
+++ AboutScreen.js
@@ -1,14 +1,38 @@
 import React from 'react';
-import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
+import { View, Text, StyleSheet, ScrollView, Image, Linking } from 'react-native';
 
 const About = () => {
   return (
     <ScrollView contentContainerStyle={styles.container}>
-      <Image source={require("./assets/about.png")} style={{ width: "100%", height: 200 }} ></Image>
+      <Image source={require("./assets/about.png")} style={{ width: "100%", height: 200 }} />
+
+      <Text style={styles.heading}>Wollongong Botanic Garden</Text>
 
       <Text style={styles.text}>
         Set on 27 hectares at the foot of Mount Keira, Wollongong Botanic Garden is a short distance from the City Centre, and next to the University of Wollongong's main campus. We are located approximately 85 kilometres south of Sydney.
       </Text>
+
+      <Image source={require("./assets/garden1.png")} style={styles.image} />
+
+      <Text style={styles.subheading}>Garden Features</Text>
+
+      <Text style={styles.text}>
+        The Garden features a diverse range of native and exotic plant species, including the Rose Garden, Woodland Garden, Palm Garden, Rainforest Collection, and the Dryland Garden. The Garden is also home to a variety of wildlife such as birds, butterflies, and reptiles.
+      </Text>
+
+      <Image source={require("./assets/garden2.png")} style={styles.image} />
+
+      <Text style={styles.subheading}>Activities and Events</Text>
+
+      <Text style={styles.text}>
+        Wollongong Botanic Garden offers a range of activities and events for visitors of all ages. These include guided walks, workshops, and special events like the popular Spring Festival and Sunset Cinema series.
+      </Text>
+
+      <Text style={styles.link} onPress={() => Linking.openURL('https://wollongongbotanicgarden.com.au/events')}>
+        Visit our events page for more details
+      </Text>
+
+      <Image source={require("./assets/garden3.png")} style={styles.image} />
 
     </ScrollView>
   );
@@ -41,6 +65,13 @@ const styles = StyleSheet.create({
     color: 'blue',
     textDecorationLine: 'underline',
     marginTop: 8,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    resizeMode: 'cover',
+    marginTop: 16,
+    marginBottom: 16,
   },
 });
 
