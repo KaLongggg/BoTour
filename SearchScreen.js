a5109615831b15c79d24c6b8991375d992f81459
Kobe666888 <127716801+Kobe666888@users.noreply.github.com>
2023-04-25 18:59:14 +0800

    Update SearchScreen.js

--- SearchScreen.js
+++ SearchScreen.js
@@ -1,47 +1,23 @@
+// SearchScreen.js
 import React, { useState } from 'react';
-import { View, Text, TextInput, StyleSheet, TouchableOpacity, FlatList } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { View, TextInput, Button, StyleSheet, Linking } from 'react-native';
 
 const SearchScreen = () => {
-  const [searchText, setSearchText] = useState('');
-  const [searchResults, setSearchResults] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   const handleSearch = () => {
-    // 在这里实现搜索功能，例如从API获取数据
-    // 这里仅模拟数据
-    const mockData = [
-      { id: '1', title: 'Result 1' },
-      { id: '2', title: 'Result 2' },
-      { id: '3', title: 'Result 3' },
-    ];
-
-    setSearchResults(mockData);
+    Linking.openURL(`https://wollongongbotanicgarden.com.au/search?query=${encodeURIComponent(searchQuery)}`);
   };
-
-  const renderItem = ({ item }) => (
-    <View style={styles.item}>
-      <Text style={styles.itemText}>{item.title}</Text>
-    </View>
-  );
 
   return (
     <View style={styles.container}>
-      <View style={styles.searchBar}>
-        <TextInput
-          style={styles.input}
-          onChangeText={setSearchText}
-          value={searchText}
-          placeholder="Search"
-        />
-        <TouchableOpacity onPress={handleSearch}>
-          <Ionicons name="ios-search" size={30} color="blue" />
-        </TouchableOpacity>
-      </View>
-      <FlatList
-        data={searchResults}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search"
+        onChangeText={setSearchQuery}
+        value={searchQuery}
       />
+      <Button title="Search" onPress={handleSearch} />
     </View>
   );
 };
@@ -49,31 +25,16 @@ const SearchScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20,
   },
-  searchBar: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    paddingHorizontal: 15,
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
     borderWidth: 1,
-    borderColor: 'blue',
-    borderRadius: 5,
-    marginTop: 10,
-    marginBottom: 20,
-  },
-  input: {
-    flex: 1,
-  },
-  item: {
-    backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
-  },
-  itemText: {
-    fontSize: 16,
+    marginBottom: 10,
+    paddingLeft: 10,
   },
 });
+
 export default SearchScreen;
