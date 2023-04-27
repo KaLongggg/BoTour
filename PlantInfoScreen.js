a7b16a58130c0cbc54851f7f2190aa1faf880dd2
KaLongggg <kalong551@gmail.com>
2023-04-27 16:07:41 +1000

    Improved plant information page and add candidate

--- PlantInfoScreen.js
+++ PlantInfoScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, ScrollView, View, Text, Image } from 'react-native';
 import plantData from './plantData';
+import { getPlantImageURI } from './TopCandidatesScreen';
+import { TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 const PlantInfoScreen = ({ route }) => {
   const { plantClass, plantProbability, photoURI } = route.params; // Get plantProbability and photoURI
   const plantInfo = plantData[plantClass];
+  const plantImageURI = getPlantImageURI(plantClass);
+  const navigation = useNavigation();
 
   if (!plantInfo) {
     return (
@@ -17,8 +22,8 @@ const PlantInfoScreen = ({ route }) => {
   }
 
   return (
-    <View style={styles.container}>
-      <Image source={{ uri: photoURI }} style={styles.plantImage} /> 
+    <ScrollView contentContainerStyle={styles.container}>
+      <Image source={plantImageURI} style={styles.plantImage} />
       <Text style={styles.title}></Text>
       <Text style={styles.plantName}>{plantClass} ({plantProbability}%)</Text>
       <Text style={styles.title}></Text>
@@ -27,7 +32,21 @@ const PlantInfoScreen = ({ route }) => {
       <Text style={styles.info}>Plant Type: {plantInfo.type}</Text>
       <Text style={styles.info}>Season of Interest: {plantInfo.seasonOfInterest}</Text>
       <Text style={styles.info}>Height: {plantInfo.height}</Text>
-    </View>
+      <TouchableOpacity
+          style={styles.button}
+          onPress={() =>
+            navigation.navigate('CapturedImage', {
+              photoURI,
+              plantClass,
+              plantProbability,
+              date: 'Example Date', // Replace with actual date and time
+              location: 'Example Location', // Replace with actual location
+            })
+          }
+        >
+          <Text style={styles.buttonText}>View your image</Text>
+        </TouchableOpacity>
+    </ScrollView>
   );
 };
 
@@ -41,17 +60,16 @@ const styles = StyleSheet.create({
   plantImage: {
     alignSelf: 'center',
     borderWidth: 2,
-    borderColor: '#000',
+    borderColor: '#ddf',
     borderRadius: 5,
     width: 336,
     height: 336,
     resizeMode: 'contain',
-    marginBottom: 10,
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 10,
   },
   plantName: {
     fontSize: 24,
@@ -72,6 +90,17 @@ const styles = StyleSheet.create({
     color: 'red',
     textAlign: 'center',
     paddingHorizontal: 10,
+  },
+  button: {
+    backgroundColor: '#007BFF',
+    padding: 10,
+    borderRadius: 5,
+    alignItems: 'center',
+    marginTop: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
   },
 });
 
---
+++ TopCandidatesScreen.js
@@ -0,0 +1,89 @@
+import React from 'react';
+import { View, Text, FlatList, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const plantImages = {
+    Lavender: require('./assets/PlantImage/lavender.jpg'),
+    Coleus: require('./assets/PlantImage/coleus.jpg'),
+    Lagerstroemia_Lipan: require('./assets/PlantImage/lagerstroemia_lipan.jpg'),
+    Cantaurea: require('./assets/PlantImage/cantaurea.jpg'),
+    Lucky_Nut: require('./assets/PlantImage/lucky_nut.jpg'),
+  };
+
+export const getPlantImageURI = (plantClass) => {
+    const formattedPlantClass = plantClass.replace(' ', '_');
+    return plantImages[formattedPlantClass];
+  };
+
+export default function TopCandidatesScreen({ route }) {
+  const { topCandidates, photoURI } = route.params;
+  console.log("Top Candidates:", topCandidates);
+  const navigation = useNavigation();
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.itemContainer}
+      onPress={() => navigation.navigate('PlantInfo', { plantClass: item.plantClass, plantProbability: (item.probability * 100).toFixed(0), photoURI })}
+    >
+      <Image source={getPlantImageURI(item.plantClass)} style={styles.smallPlantImage} />
+      <Text style={styles.itemText}>{`${item.plantClass}: ${(item.probability * 100).toFixed(0)}%`}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Image source={{ uri: photoURI }} style={styles.plantImage} />
+      <Text style={styles.title}>
+        Prediction Result:
+      </Text>
+      <FlatList
+        data={topCandidates}
+        renderItem={renderItem}
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingHorizontal: 10,
+    paddingTop: 20,
+    paddingBottom: 30,
+  },
+  itemContainer: {
+    backgroundColor: '#f5f5f2',
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  itemText: {
+    fontSize: 18,
+  },
+  title:{
+    fontSize: 24,
+  },
+  plantImage: {
+    alignSelf: 'center',
+    borderWidth: 2,
+    borderColor: '#ddf',
+    borderRadius: 8,
+    width: 224,
+    height: 224,
+    resizeMode: 'contain',
+    marginBottom: 10,
+  },
+  smallPlantImage: {
+    width: 112,
+    height: 112,
+    borderRadius: 5,
+    resizeMode: 'contain',
+  },
+});
